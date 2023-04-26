a6046b10dfe99edeed8af2c4a69b8308a72720d4
Kamil <kkuzmicki@wp.pl>
2023-04-26 17:00:56 +0200

    Working downsampler

--- additional_utils.py
+++ additional_utils.py
@@ -12,12 +12,16 @@ def downsample_tracks(directory, destination):
 
         for file_name in files:
             original_file_directory = root + '\\' + file_name
+
+            os.makedirs(root.replace(directory, destination), exist_ok=True)
             target_file_directory = root.replace(directory, destination) + '\\' + file_name
 
-            y, s = soundfile.read(original_file_directory)
+            #y, s = soundfile.read(original_file_directory)
+            y, s = librosa.load(original_file_directory, sr=16000) # librosa used because of possibility to set samplerate, there is downsampling to 16kHz
             print('y: ', y)
 
-            soundfile.write(target_file_directory, y, samplerate=16000)
+            soundfile.write(target_file_directory, y, samplerate=16000) # this samplerate doesn't downsample!
+            print('TRACK\'S PATH: ', target_file_directory)
 
 
 
